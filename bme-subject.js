2d799ad96c5ab2e0ddeeba19d54ebdf19b1298a7
jonkom7 <gergely.daniel7@gmail.com>
2017-03-21 03:32:29 +0100

    JSON alapú REST API-s adatbeolvasás

--- bme-subject.js
+++ bme-subject.js
@@ -1,7 +1,7 @@
 var cmp = Vue.component('bme-subject', {
 	template : 	'<div>\
 	<template v-for="subj in subjects">\
-	<div class="subjcontainer" id="{{subj.code}}" draggable="true">\
+	<div class="subjcontainer" v-bind:id="subj.code" draggable="true">\
 		<div class="subject">\
 			{{subj.short}} ({{subj.credit}})\
 		</div>\
@@ -18,53 +18,21 @@ var cmp = Vue.component('bme-subject', {
 	</div>',
 	data : function () {
 		return {
-			"subjects" :[
-			{
-				"code": "BMETE90AX00",
-				"name": "Matematika A1a",
-				"short": "Matematika A1a",
-				"exam": "1",
-				"credit": "6",
-				"term": "1",
-				"crossterm": "0"
-			}, 
-			{
-				"code": "BMETE90AX02",
-				"name": "Matematika A2a",
-				"short": "Matematika A2a",
-				"exam": "1",
-				"credit": "6",
-				"term": "2",
-				"crossterm": "0"
-			}, 
-			{
-				"code": "BMETE90AX10",
-				"name": "Matematika A3a",
-				"short": "Matematika A3a",
-				"exam": "0",
-				"credit": "4",
-				"term": "3",
-				"crossterm": "0"
-			},
-			{
-				"code": "BMETE15AX02",
-				"name": "Fizika A2",
-				"short": "Fizika A2",
-				"exam": "1",
-				"credit": "2",
-				"term": "2",
-				"crossterm": "0"
-			},
-			{
-				"code": "BMEGEMTAMT1",
-				"name": "Anyagismeret",
-				"short": "Anyagismeret",
-				"exam": "1",
-				"credit": "5",
-				"term": "1",
-				"crossterm": "0"
-			}
-		]};
+			subjects : {}
+		};
+	},
+	mounted : function (){
+		console.log('lol')
+		this.fetchx();
+	},
+	methods : {
+		fetchx : function(){
+			var that = this;
+			Vue.http.get('./subjects.json').then(function(res){
+				that.subjects = res.body.subjects;
+				console.log(res.body.subjects);
+			});
+		}
 	}
 });
 
