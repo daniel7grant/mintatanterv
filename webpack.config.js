1b9e6b9bdd8a55f47548990e8c73be69715b277b
daniel7grant <gergely.daniel7@gmail.com>
2017-08-19 11:37:14 +0200

    Clean up old and leftover Vue-CLI files and organize css

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      // sourceMap: true,
       compress: {
         warnings: false
       }
